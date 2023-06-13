21510b0c43118a08435ddef35d72da29e54ab943
Shibatataichi <shibakenta.xx@gmail.com>
2023-06-13 17:34:17 +0900

    laravel portforio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>芝多たいち|HOME</title>
+        <meta name="description" content="Vtuber、音楽有償依頼、MIX、楽曲依頼、配信">
+        <link rel="stylesheet" href="CSS/style.css">
+    </head>
+    <body>
+        <header class="page-header wrapper">
+            <img class="logo" src="images/rogo.png" alt="rogo">
+            <nav>
+                <ul class="main-nav">
+                <li><a href="">about</a></li>
+                    <li><a href="">works</a></li>
+                        <li><a href="https://twitter.com/shibata_taichi">SNS</a></li>
+                            <li><a href="">contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <div class="home-content wtapper">
+                    <p>Vtuber,<br>
+                        composer,<br>
+                        streamer,<br>
+                        Mixer,<br>
+                        and more...
+                    </p>
+                </div>
+</body>
+</html>
